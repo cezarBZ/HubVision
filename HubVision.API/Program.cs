133a7184ee3e790e1b48b3490e2391ef82d5080d
Cézar Bezerra <cezar30santos@gmail.com>
2025-10-08 09:29:55 -0300

    feat: add ApplicationDbContext

--- HubVision.API/Program.cs
+++ HubVision.API/Program.cs
@@ -1,3 +1,6 @@
+using HubVision.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+           options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"
+            ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
 var app = builder.Build();
 
---
+++ HubVision.Infrastructure/Data/ApplicationDataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace HubVision.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public const string DEFAULT_SCHEMA = "HV";
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
